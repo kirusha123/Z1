fddf108e467349993876b31108e26d2cfd682527
kirusha123 <kirriess@mail.ru>
2021-07-27 22:39:35 +0300

    updated docs file

---
+++ docs/4738a8c59741311f9c13.js
@@ -0,0 +1,1 @@
+function init(){let e=new ymaps.Map("YMapsID",{center:[56.75,37.16],zoom:7}),t=new ymaps.GeoObject({geometry:{type:"Point",coordinates:[56.753450272088116,37.16410160155168]},properties:{iconContent:"Temple of Web Development",hintContent:"ABOBA WEBDEV"}},{preset:"islands#blackStretchyIcon",draggable:!1});e.geoObjects.add(t)}ymaps.ready(init);
